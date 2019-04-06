0ce597d9e219a04712f14665d1942a2cef0b8c9d
Александр Гарькавенко <>
2019-04-06 11:42:18 +0400

    test debug for server client

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("hello mad mad wordl!!!");
+const net = require('net');
+var server = net.createServer((socket)=>{
+    let rawResponse;
+    socket.on('data', function(chunk) {
+        console.log(chunk);
+        socket.write(chunk);
+    });
+
+});
+server.listen(1610, "127.0.0.1");
+server.listen(616, "46.101.116.209");
